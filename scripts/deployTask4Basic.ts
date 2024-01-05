cb450440f9c0c9c43cdff4bcbbf7c7492d331a0f
Aiomedin Chorobaev <eliothab58@gmail.com>
2024-01-06 01:24:01 +0600

    4 b

--- scripts/deployTask4Basic.ts
+++ scripts/deployTask4Basic.ts
@@ -5,9 +5,11 @@ import { compile, NetworkProvider } from '@ton-community/blueprint';
 export async function run(provider: NetworkProvider) {
     const task4Basic = provider.open(Task4Basic.createFromConfig({}, await compile('Task4Basic')));
 
-    await task4Basic.sendDeploy(provider.sender(), toNano('20'));
+    await task4Basic.sendDeploy(provider.sender(), toNano('0.05'));
 
     await provider.waitForDeploy(task4Basic.address);
 
-    console.log((await task4Basic.getSolve()))
+    let stack = await task4Basic.getSolve()
+
+    console.log(stack.readNumber(), stack.readNumber(), stack.readNumber(), stack.readTuple())
 }
